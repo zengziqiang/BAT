3b817b5ff4d26d2aa60402cf63dde058a969deb9
zengziqiang <272804553@qq.com>
2020-12-19 11:30:42 +0800

    后台项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BAT
-这将是一个伟大的项目！
+## 目录介绍
+
+| 序号 | 目录名 | 描述 |
+|:--|:--|:--|
+| 一 | bat-admin | 后台管理 |
+| 二 | bat-web | 前端项目 |
+| 三 | bat-wx-program | 微信小程序 |
+| 四 | bat-android | Android项目 |
+| 五 | bat-ios | iOS项目 |
---
+++ bat-admin/bat-oauth/src/main/java/com/bat/BatOauthApplication.java
@@ -0,0 +1,25 @@
+package com.bat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@RestController
+public class BatOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatOauthApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String indexMsg(HttpServletRequest request) {
+        String addr = request.getRemoteAddr();
+        int port = request.getLocalPort();
+        return LocalDateTime.now().toString() + "####addr:" + addr + "####port:" + port;
+    }
+}
